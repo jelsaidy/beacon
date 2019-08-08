9aa0a3e6d449fc2a8f72c8d73185b41df2e58a62
Jacob <j>
2019-08-08 23:36:12 +1000

    Initial Commit

---
+++ Beacon1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Beacon1
+//
+//  Created by Jacob El-Saidy on 8/8/19.
+//  Copyright © 2019 Jacob El-Saidy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
